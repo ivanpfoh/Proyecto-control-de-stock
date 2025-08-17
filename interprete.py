f1d84eb2e3863c2c78491627bd4a6d38bfd2464f
ivanpfoh <ivanpfohh85@gmail.com>
2025-08-17 19:09:40 -0300

    msg: Updates files

--- interprete.py
+++
@@ -1,8 +0,0 @@
-import sqlite3
-
-conexion = sqlite3.connect("stock.db")
-cursor = conexion.cursor()
-cursor.execute("PRAGMA table_info(productos)")
-for columna in cursor.fetchall():
-    print(columna)
-conexion.close()
